fecc03d0902f62483dc38b40954538704a116ba1
KokokAyam <75647574+KokokAyam@users.noreply.github.com>
2020-12-29 07:04:41 +0700

    initial commit

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1609200175,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/circle-sheet0.png",
+		"images/circle-sheet1.png",
+		"images/circle-sheet2.png",
+		"images/knife-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/bg-sheet1.png",
+		"images/item-sheet0.png",
+		"images/playbtn-sheet0.png",
+		"images/particles.png",
+		"images/logo-sheet0.png",
+		"images/toplogo-sheet0.png",
+		"media/collision.ogg",
+		"media/explode.ogg",
+		"media/open.ogg",
+		"media/pindah.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
